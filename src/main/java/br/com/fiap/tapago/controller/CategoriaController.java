72d55291f209bece1ea586fa8ac894bcd490d39b
mucaumo <murilo.caumo.murgia@gmail.com>
2024-03-14 11:47:01 -0300

    feat: complete refatorated controller for now

--- src/main/java/br/com/fiap/tapago/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/tapago/controller/CategoriaController.java
@@ -1,14 +1,16 @@
 package br.com.fiap.tapago.controller;
 
-import java.util.ArrayList;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,100 +18,71 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.tapago.model.Categoria;
+import br.com.fiap.tapago.repository.CategoriaRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("categoria")
+@Slf4j
 public class CategoriaController {
 
     Logger log = LoggerFactory.getLogger(getClass());
 
-    List<Categoria> repository = new ArrayList<>();
+    @Autowired // Injeção de Dependência - Inversão de Controle
+    CategoriaRepository repository;
 
     @GetMapping
     public List<Categoria> index() {
-        return repository;
+        return repository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<Categoria> create(@RequestBody Categoria categoria) {
+    @ResponseStatus(CREATED)
+    public Categoria create(@RequestBody Categoria categoria) {
         log.info("Cadastrando categoria {}", categoria);
-        repository.add(categoria);
-        return ResponseEntity.status(HttpStatus.CREATED).body(categoria);
+        return repository.save(categoria);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Categoria> show(@PathVariable Long id) {
         log.info("buscando categoria com id {}", id);
 
-        // for(Categoria categoria: repository){
-        // if (categoria.id().equals(id))
-        // return ResponseEntity.status(HttpStatus.OK).body(categoria);
-        // }
-
-        var categoriaEncontrada = getCategoriaById(id);
-
-        if (categoriaEncontrada.isEmpty())
-            return ResponseEntity.notFound().build();
-
-        return ResponseEntity.ok(categoriaEncontrada.get());
+        return repository
+            .findById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
 
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+    @ResponseStatus(NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
         log.info("apagando categoria {}", id);
 
-        var categoriaEncontrada = getCategoriaById(id);
-
-        if (categoriaEncontrada.isEmpty())
-            return ResponseEntity.notFound().build();
-
-        repository.remove(categoriaEncontrada.get());
-
-        return ResponseEntity.noContent().build();
+        verificarSeCategoriaExiste(id);
+            repository.deleteById(id);
     }
-
 
     @PutMapping("{id}")
-    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody Categoria categoria){
+    public Categoria update(@PathVariable Long id, @RequestBody Categoria categoria){
         log.info("atualizar categoria {} para {}", id, categoria);
 
-        // buscar a categoria antiga -> 404
-        var categoriaEncontrada = getCategoriaById(id);
+        verificarSeCategoriaExiste(id);
 
-        if (categoriaEncontrada.isEmpty())
-            return ResponseEntity.notFound().build();
-
-        var categoriaAntiga = categoriaEncontrada.get();
-
-        // criar a categoria nova com os dados atualizados
-        var categoriaNova = new Categoria(id, categoria.nome(), categoria.icone());
-
-        // apagar a categoria antiga
-        repository.remove(categoriaAntiga);
-
-        // add a categoria nova
-        repository.add(categoriaNova);
-
-        return ResponseEntity.ok(categoriaNova);
+        categoria.setId(id);
+        return repository.save(categoria);
     }
 
-
-
-
-
-    private Optional<Categoria> getCategoriaById(Long id) {
-        var categoriaEncontrada = repository
-                .stream()
-                .filter(c -> c.id().equals(id))
-                .findFirst();
-        return categoriaEncontrada;
+    private void verificarSeCategoriaExiste(Long id) {
+        repository
+            .findById(id)
+            .orElseThrow(() -> new ResponseStatusException(
+                NOT_FOUND, "Não existe categoria com o id"));
     }
 
 }
